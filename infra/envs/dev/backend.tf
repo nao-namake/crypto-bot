27c4152816b0a4e81b9e19ba01e2ed0f6c702805
nao <s00198532@gmail.com>
2025-06-16 06:51:50 +0900

    docs: note GCS backend perms

--- infra/envs/dev/backend.tf
+++ infra/envs/dev/backend.tf
@@ -1,3 +1,8 @@
+#---------------------------------------------------------------------------------
+# Remote backend
+#   • GCS bucket must exist **before** terraform init
+#   • SA (github-deployer) needs roles/storage.objectAdmin (or equivalent)
+#---------------------------------------------------------------------------------
 terraform {
   backend "gcs" {
     bucket = "my-crypto-bot-tfstate"
