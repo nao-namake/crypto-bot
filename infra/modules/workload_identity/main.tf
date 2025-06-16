699513a28d9d1ed2a54ab8b8296c560120d60cf6
nao <s00198532@gmail.com>
2025-06-17 05:31:43 +0900

    fix: add SA Viewer role so Terraform can read getIamPolicy

--- infra/modules/workload_identity/main.tf
+++ infra/modules/workload_identity/main.tf
@@ -22,7 +22,7 @@ resource "google_iam_workload_identity_pool_provider" "provider" {
     "attribute.repository" = "assertion.repository"
     "attribute.ref"        = "assertion.ref"
   }
-  # NOTE: keep using the variable so other environments can override.
+  # NOTE: Keep using the variable so other environments can override.
   # default value is defined in variables.tf
   attribute_condition = "attribute.repository == \"${var.github_repo}\""
 
@@ -31,9 +31,14 @@ resource "google_iam_workload_identity_pool_provider" "provider" {
 
 
 #######################################
-# Service‑Account ↔ WIF プリンシパル紐付け
-# GitHub Actions → Workload Identity → SA impersonation
+# SA に ServiceAccountViewer を付与（getIamPolicy を許可）
 #######################################
+resource "google_service_account_iam_member" "deployer_sa_viewer" {
+  service_account_id = "projects/${var.project_id}/serviceAccounts/${var.deployer_sa}"
+  role               = "roles/iam.serviceAccountViewer"
+  member             = "serviceAccount:${var.deployer_sa}"
+}
+
 resource "google_service_account_iam_member" "wif_binding" {
   service_account_id = "projects/${var.project_id}/serviceAccounts/${var.deployer_sa}"
   role               = "roles/iam.workloadIdentityUser"
@@ -43,19 +48,18 @@ resource "google_service_account_iam_member" "wif_binding" {
   member = "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.pool.workload_identity_pool_id}/attribute.repository/${var.github_repo}"
 }
 
-#--------------------------------------------------
-# SA に Workload‑Identity Pool Admin を付与
-#   ※ Pool / Provider を Terraform で管理するために必要
-#--------------------------------------------------
+#######################################
+# SA に Workload‑Identity Pool Admin を付与（Pool/Provider を Terraform 管理出来るように）
+#######################################
 resource "google_project_iam_member" "deployer_sa_wip_admin" {
   project = var.project_id
   role    = "roles/iam.workloadIdentityPoolAdmin"
   member  = "serviceAccount:${var.deployer_sa}"
 }
 
-#--------------------------------------------------
-# SA 自身に ServiceAccountAdmin を付与
-#--------------------------------------------------
+#######################################
+# SA に ServiceAccountAdmin を付与（自身の IAM を更新出来るように）
+#######################################
 resource "google_project_iam_member" "deployer_sa_admin" {
   project = var.project_id
   role    = "roles/iam.serviceAccountAdmin"
