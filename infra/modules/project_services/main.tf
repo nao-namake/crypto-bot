4b3b79ffde892b94df0caa794ef532434e22aa7e
nao <s00198532@gmail.com>
2025-06-19 06:43:18 +0900

    ci: push Docker image to GCP Artifact Registry as well

--- infra/modules/project_services/main.tf
+++ infra/modules/project_services/main.tf
@@ -4,3 +4,9 @@ resource "google_project_service" "enabled" {
   service            = each.value
   disable_on_destroy = true
 }
+
+resource "google_project_iam_member" "run_sa_ar_reader" {
+  project = var.project_id
+  role    = "roles/artifactregistry.reader"
+  member  = "serviceAccount:service-${var.project_number}@serverless-robot-prod.iam.gserviceaccount.com"
+}
